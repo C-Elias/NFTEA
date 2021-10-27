d53ef6d7c0e30caed73e848d2231e729a237b3d3
Gustavo Meneses <gdameneses@gmail.com>
2021-10-27 02:00:20 +0100

    index and show finished

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,5 +1,15 @@
 class NftsController < ApplicationController
+  before_action :get_nft, only: [:show]
   def index
     @nfts = Nft.all
+  end
+
+  def show
+  end
+
+  private
+
+  def get_nft
+    @nft = Nft.find(params[:id])
   end
 end
