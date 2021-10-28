6a2798dab389b4ae0fb7118ad820780f35222031
Barbara Santos <barbaransantos1@gmail.com>
2021-10-28 20:58:26 -0300

    wip

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -1,15 +1,41 @@
 class NftsController < ApplicationController
-  before_action :get_nft, only: [:show]
+ before_action :get_nft, only: [:show, :destroy]
+
   def index
-    @nfts = Nft.all
+    # @nfts = Nft.all
+    @nfts = policy_scope(Nft).order(created_at: :desc)
   end
 
-  def show
-  end
+  # def show
+  # end
+
+  # def new
+  #   @nft = Nft.new
+  #   authorize @nft
+  # end
+  # # POST /nfts
+  # def create
+  #   @nft = Nft.new(nft_params)
+  #   @nft.user = current_user
+  #   authorize @nft
+  #   if @nft.save
+  #     redirect_to @nft, notice: 'NFT was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+  # # DELETE /nft/1
+  # def destroy
+  #   @nft.destroy
+  #   redirect_to nft_url, notice: 'NFT was successfully deleted.'
+  # end
 
   private
 
   def get_nft
-    @nft = Nft.find(params[:id])
+   @nft = Nft.find(params[:id])
   end
+  # def nft_params
+  #   params.require(:nft).permit(:user)
+  # end
 end
